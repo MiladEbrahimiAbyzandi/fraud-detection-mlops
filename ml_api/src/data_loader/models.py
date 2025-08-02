6ef70702297f5f5a5849f13d608b8e79e2532677
Milad Ebrahimi Abyazandi <150760233+MiladEbrahimiAbyzandi@users.noreply.github.com>
2025-08-02 16:12:55 -0400

    feat: create transformation_stage_1.py and model.py for first-stage transformation; update models.py (#2)

--- ml_api/src/data_loader/models.py
+++ ml_api/src/data_loader/models.py
@@ -1,6 +1,8 @@
 from pydantic import BaseModel, field_validator
 import numpy as np
-from pydantic import BaseModel, field_validator, FieldValidationInfo
+import pandas as pd
+from datetime import datetime
+from pydantic import BaseModel, field_validator,model_validator, FieldValidationInfo
 
 
 class Card(BaseModel):
@@ -9,12 +11,12 @@ class Card(BaseModel):
     Card_Brand: str
     Card_Type: str
     Card_Number: int
-    Expires: str
+    Expires: datetime | None
     CVV: int
     Has_Chip: str
     Cards_Issued: int
-    Credit_Limit: str
-    Acct_Open_Date: str
+    Credit_Limit: float | None
+    Acct_Open_Date: datetime | None
     Year_PIN_last_Changed: int
     Card_on_Dark_Web: str
 
@@ -23,6 +25,29 @@ class Card(BaseModel):
         if v is np.nan:
             return None
         return v
+    
+    @field_validator("Acct_Open_Date","Expires", mode='before')
+    def parse_date(cls,v):
+        if v is None or "":
+            return None
+        if isinstance(v,datetime):
+            return v
+        try:
+            return datetime.strptime(v, "%m/%Y")
+        except ValueError:
+            raise ValueError(f"Invalid date format for value: {v}, expected 'YYYY-MM-DD'")
+
+
+    @field_validator("Credit_Limit", mode='before')
+    def validate_credit_limit(cls,v):
+        if v is None:
+            return None
+        if isinstance(v, str):
+            v = v.replace("$", "").replace(",", "")
+        try:
+            return float(v)
+        except ValueError:
+            raise ValueError(f"Invalid credit limit value: {v}")
 
 
 class User(BaseModel):
@@ -39,15 +64,28 @@ class User(BaseModel):
     Zipcode: str
     Latitude: float
     Longitude: float
-    Per_Capita_Income_Zipcode: str
-    Yearly_Income_Person: str
-    Total_Debt: str
+    Per_Capita_Income_Zipcode: float | None
+    Yearly_Income_Person: float | None
+    Total_Debt: float | None
     FICO_Score: int
     Num_Credit_Cards: int
 
     @field_validator("Zipcode", mode="before")
     def validate_zipcode(cls, v):
         return str(v)
+    
+    @field_validator("Per_Capita_Income_Zipcode","Yearly_Income_Person","Total_Debt", mode='before')
+    def validate_monetary_fields_in_user(cls,v):
+        if v is None:
+            return None
+        if isinstance(v, str):
+            v = v.replace("$", "").replace(",", "")
+        try:
+            return float(v)
+        except ValueError:
+            raise ValueError(f"Invalid monetary value: {v}")
+
+
 
     @field_validator("Apartment", mode="before")
     def validate_apartment(cls, v):
@@ -71,22 +109,82 @@ class Transaction(BaseModel):
     Month: int
     Day: int
     Time: str
-    Amount: str
+    Amount: float |None
     Use_Chip: str
-    Merchant_Name: int | None
+    Merchant_Name: str | None
     Merchant_City: str | None
     Merchant_State: str | None
-    Zip: float
+    Zip: str
     MCC: int
     Errors: str | None = None
-    Is_Fraud: str
+    Is_Fraud: int | str
 
     @field_validator("*", mode="before")
     def validate_all(cls, v):
-        if v is np.nan:
+        if pd.isna(v):
             return None
         return v
+    
+    @field_validator("Amount", mode='before')
+    def validate_Amount(cls,v):
+        if v is None:
+            return None
+        if isinstance(v, str):
+            v = v.replace("$", "").replace(",", "")
+        try:
+            return float(v)
+        except ValueError:
+            raise ValueError(f"Invalid Amount value: {v}")
+        
+    @field_validator("Errors", mode='before')
+    def fillna_errors(cls, v):
+        if v is None or v == '':
+            return "No Error"
+        return v
+    
+    @field_validator("Merchant_Name", mode="before")
+    def validate_Merchant_Name(cls, v):
+        return str(v)
+    
+    @field_validator ("Is_Fraud", mode='before')
+    def convert_is_fraud(cls,v):
+        mapping={'yes':1,'no':0}
+        if isinstance(v,str):
+            v_lower=v.lower()
+            if v_lower in mapping:
+                return mapping[v_lower]
+            else:
+                raise ValueError(f"Invalid string for Is_Fraud: {v}")
+        elif isinstance(v,int):
+            if v in (0,1):
+                return v
+            else:
+                raise ValueError (f"Is_Fraud int must be 0 or 1: {v}")
+        else:
+            raise TypeError (f"Is_Fraud must be int or str, got: {type(v)}")
 
+    @field_validator("Zip", mode='before')
+    def validate_Zip(cls,v):
+        if pd.isna(v) or v=='':
+            return "Unknown"
+        elif isinstance(v,float):
+            return str(int(v))
+        else:
+            return str(v)
+    
+    @model_validator(mode='before')
+    def set_state_if_city_online (cls,data):
+        city=data.get("Merchant_City")
+        if city.lower() =='online':
+            data["Merchant_State"]= 'online'
+        return data
+    
+    @model_validator(mode='before')
+    def set_xip_if_city_online (cls,data):
+        city=data.get("Merchant_City")
+        if city.lower() =='online':
+            data["Zip"]= 'online'
+        return data
 
 class RawData(BaseModel):
     cards: list[Card]
---
+++ ml_api/src/transformation_stage1/transformation_stage_1.py
@@ -0,0 +1,202 @@
+import pandas as pd
+import numpy as np
+from transformation_stage1.model import TransactionFeatures
+
+
+def transform_stage1(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Perform first stage of data transformation and feature engineering
+    Return pandas dataframe
+    """
+
+    # Convert categorical columns to category type
+    categorical_cols = [
+                "Card_Type",
+                "Gender",
+                "Card_Brand",
+                "Use_Chip",
+                "Has_Chip",
+                "Card_on_Dark_Web",
+            ]
+    for col in categorical_cols:
+                df[col] = df[col].astype("category")
+
+    # Create timestamp
+    df["datetime_str"] = (
+        df["Year"].astype(str)
+        + "-"
+        + df["Month"].astype(str).str.zfill(2)
+        + "-"
+        + df["Day"].astype(str).str.zfill(2)
+        + " "
+        + df["Time"]
+    )
+    df["timestamp"] = pd.to_datetime(
+        df["datetime_str"], format="%Y-%m-%d %H:%M"
+    )
+    df.drop(["datetime_str"], axis=1, inplace=True)
+
+    # Transaction Hour
+    df["transaction_hour"] = pd.to_datetime(
+        df["Time"], format="%H:%M"
+    ).dt.hour
+
+    #  Date and Day of Week
+    df["Date"] = pd.to_datetime(
+        df[["Year", "Month", "Day"]].astype(str).agg("-".join, axis=1),
+        errors="coerce",
+    )
+    df["day_of_week"] = df["Date"].dt.day_name()
+
+    # Transaction Type
+    df["transaction_type"] = df["Merchant_City"].apply(
+        lambda x: "CNP" if x.strip().lower() == "online" else "CP"
+    )
+
+    # Age-related features
+    df["age_group"] = pd.cut(
+        df["Current_Age"],
+        bins=[0, 20, 30, 40, 50, 60, 70, 110],
+        labels=["<20", "20-30", "30-40", "40-50", "50-60", "60-70", "70+"],
+    )
+    df["is_retired"] = (
+        df["Retirement_Age"] <= df["Current_Age"]
+    ).astype(int)
+    df["years_to_retirement"] = np.where(
+        df["is_retired"] == 0,
+        df["Retirement_Age"] - df["Current_Age"],
+        np.nan,
+    )
+    df["retirement_proximity"] = pd.cut(
+        df["years_to_retirement"] ,
+        bins=[0, 5, 10, 20, np.inf],
+        labels=["0-5", "5-10", "10-20", "20-inf"],
+    )
+    df["years_since_retirement"] = np.where(
+        df["is_retired"] == 1,
+        -df["Retirement_Age"] + df["Current_Age"],
+        np.nan,
+    )
+    df["retirement_phase"] = pd.cut(
+        df["years_since_retirement"],
+        bins=[0, 5, 10, 20, np.inf],
+        labels=["0-5", "5-10", "10-20", "20-inf"],
+    )
+
+    # Income and zip tiers
+    df["zip_income_tier"] = pd.cut(
+        df["Per_Capita_Income_Zipcode"],
+        bins=[0, 30000, 60000, 100000, 200000, np.inf],
+        labels=["Very Low", "Low", "Medium", "High", "Very High"],
+    )
+    df["income_tier"] = pd.cut(
+        df["Yearly_Income_Person"],
+        bins=[0, 25000, 50000, 100000, 250000, np.inf],
+        labels=["<25K", "25K-50K", "50K-100K", "100K-250K", "250K+"],
+    )
+    df["amount_income_ratio"] = np.where(
+        df["Yearly_Income_Person"].notna() & (df["Yearly_Income_Person"] != 0),
+        df["Amount"] / df["Yearly_Income_Person"],
+        np.nan
+    )
+    df["income_relative_to_zip"] = np.where(
+        df["Per_Capita_Income_Zipcode"].notna() & (df["Per_Capita_Income_Zipcode"] != 0),
+        df["Yearly_Income_Person"] / df["Per_Capita_Income_Zipcode"],
+        0
+    )
+
+    df["income_mismatch"] = pd.cut(
+        df["income_relative_to_zip"],
+        bins=[0, 0.5, 0.8, 1.2, 2, np.inf],
+        labels=["<50%", "50-80%", "80-120%", "120-200%", ">200%"],
+    )
+
+    # --------------------
+    df["debt_to_income"] = np.where(
+        df["Yearly_Income_Person"].notna() & (df["Yearly_Income_Person"] != 0),
+        df["Total_Debt"] / df["Yearly_Income_Person"],
+        np.nan
+    )
+
+    df["high_debt_ratio"] = np.where(
+        df["debt_to_income"] > 2 & (df["debt_to_income"].notna()),
+        1,
+        0
+    )
+    df["high_debt_high_spend"] = (
+        (df["Total_Debt"] > 0.5 * df["Yearly_Income_Person"])
+        & (df["Amount"] > 0.1 * df["Yearly_Income_Person"])
+    ).astype(int)
+    df["credit_utilization"] = (
+        df["Total_Debt"] / df["Credit_Limit"]
+    )
+    df["credit_utilization"] = df["credit_utilization"].replace(
+        [np.inf, -np.inf], np.nan
+    )
+    df["credit_util_bin"] = pd.cut(
+        df["credit_utilization"],
+        bins=[0, 0.1, 0.3, 0.5, 0.7, np.inf],
+        labels=["Very Low", "Low", "Medium", "High", "Maxed Out"],
+    )
+    df["fico_tier"] = pd.cut(
+        df["FICO_Score"],
+        bins=[300, 580, 670, 740, 800, 850],
+        labels=[
+            "Poor (<580)",
+            "Fair (580-669)",
+            "Good (670-739)",
+            "Very Good (740-799)",
+            "Exceptional (800+)",
+        ],
+    )
+    df["low_FICO_high_spend"] = (
+        (df["FICO_Score"] < 671)
+        & (df["Amount"] > 0.1 * df["Yearly_Income_Person"])
+    ).astype(int)
+    df["low_FICO_high_DTI"] = (
+        (df["FICO_Score"] < 670) & (df["debt_to_income"] > 2)
+    ).astype(int)
+
+    # Synthetic fraud
+    df["account_tenure_years"] = (
+        -(df["Acct_Open_Date"] - df["timestamp"]).dt.days / 365
+    )
+    df["synthetic_risk"] = (
+        (df["FICO_Score"] >= 740) & (df["account_tenure_years"] < 2)
+    ).astype(int)
+    df["synthetic_risk_2"] = (
+        (df["Num_Credit_Cards"] > 4) & (df["high_debt_ratio"] == 1)
+    ).astype(int)
+    # Zip mismatch
+    df["is_us_zip"] = df["Zip"].str.match(r"^\d{4,5}$")
+    df["zip_mismatch_flag"] = (
+        (df["Zip"] != df["Zipcode"]) & df["is_us_zip"]
+    ).astype(int)
+
+    # Expiry
+    df["months_to_expiry"] = (
+        df["Expires"] - df["timestamp"]
+    ).dt.days / 30
+
+    # Validate the transformed data
+    validated = [TransactionFeatures(**row) for row in df.to_dict(orient="records")]
+    df= pd.DataFrame([v.model_dump() for v in validated])
+
+    return df
+
+
+if __name__ == "__main__":
+    # Example usage
+    from data_loader.load_data import load_data
+    from merge_csvs.merge_data import merge_csvs
+    data= load_data()
+    df= merge_csvs(data.cards, data.users, data.transactions)
+    df=transform_stage1(df)
+    print (df)
+    
+
+
+    
+
+
+
